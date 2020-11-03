7a2a83fe8eeecfba2ae4ee13814e062cad033891
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-04 02:20:26 +0600

    Update Java 2: variable.java

--- Java 2: variable.java
+++ Java 2: variable.java
@@ -1,1 +1,22 @@
 
+package problem;
+public class problem {
+    public static void main(String[] args) {
+        
+        boolean b = true;  // boolean variable 
+        byte by = 122;      // range is -128 to 127 (-27 to 27 – 1)
+        short s = 4536;    // 16-bit signed range  range is -32768 to 32767 (or -215 to 215 – 1).
+        int n = 356443;     // 32-bit signed range from -2,147,483,648 to 2,147,4 83,647 (-231 to 231– 1).
+        long x = 23423523;  // range -263 to 263– 1.
+        char ch = 'a';      // range of the char data type is 0 to 65535.
+        String str = "md shohed"; 
+        
+        System.out.println("Boolean: "+b);
+        System.out.println("byte: "+by);
+        System.out.println("Short: "+s);
+        System.out.println("Integer: "+n);
+        System.out.println("LOng value: "+x);
+        System.out.println("Charecter: "+ch);
+        System.out.println("String: "+str);
+    }   
+}
