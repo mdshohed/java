fc228ffa250203a99c719a7aade4940d3bd8285f
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-04 10:49:48 +0600

    Update Java 3: Loop.java

--- Java 3: Loop.java
+++ Java 3: Loop.java
@@ -1,1 +1,32 @@
 
+package problem;
+
+import java.util.*;
+public class problem {
+    public static void main(String[] args) {
+        
+        //for loop
+        int n = 10; 
+        for (int i = 0; i<n; i++) {
+            System.out.println("value :"+i);
+        }
+         System.out.println("\n");
+        // while loop 
+        int i = 0;
+        while ( i<n) {
+            System.out.println("value: "+i);
+            i++;
+        }
+        System.out.println("\n");
+
+
+        // do while loop 
+        i = 0; 
+        do{
+             i++;
+             if ( i%2==0) {
+               System.out.println("do while: "+i);
+             }
+        }while(i<=n);
+    }   
+}
