2471ad9cd2ed990f736dd6d26e6f0cba4490e093
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-04 12:53:26 +0600

    Update Java 7: Math.java

--- Java 7: Math.java
+++ Java 7: Math.java
@@ -1,16 +1,18 @@
-public  class first{
+package problem;
+
+public  class problem{
     public static void main(String[] args) {
         // Math all function use ...
-        System.out.println(Math.abs(-50));
-        System.out.println(Math.sqrt(25));
-        System.out.println(Math.pow(2,3));
-        System.out.println(Math.PI);
-        System.out.println(Math.log(3));
-        System.out.println(Math.exp(1));
-        System.out.println(Math.max(100, 50));
-        System.out.println(Math.min(100, 50));
-        System.out.println(Math.ceil(-65.6));
-        System.out.println(Math.floor(5));
-        System.out.println(Math.round(4.4));
+        System.out.println("Absulate value: "+Math.abs(-50));
+        System.out.println("Sqrt value: "+Math.sqrt(25));
+        System.out.println("power of vlaue: "+Math.pow(2,3));
+        System.out.println("pi value: "+Math.PI);
+        System.out.println("log value: "+Math.log(3));
+        System.out.println("exp vlaue: "+Math.exp(1));
+        System.out.println("Maximum value: "+Math.max(100, 50));
+        System.out.println("Minimum value: "+Math.min(100, 50));
+        System.out.println("Ceil of value: "+Math.ceil(-65.6));
+        System.out.println("floor value: "+Math.floor(5));
+        System.out.println("Round value: "+Math.round(4.4));
     }
 }
