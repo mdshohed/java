5f45c70e17bc680bd6c5d134d5423700e1aa2ece
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-05 01:41:22 +0600

    Update throw & throws.java

--- Java 6; Exception handing/throw & throws.java
+++ Java 6; Exception handing/throw & throws.java
@@ -1,1 +1,20 @@
 
+
+package Javacode;
+
+import java.io.*;
+
+public class Codechef{
+	public static Void findfile() throws IOException {   // single or multiple vlaue use throws and throw use what type messege for throws exception
+		File file = new File("in.txt"); 
+		throw new IOException("not found");
+	}
+	public static void main(String[] args) {
+		try {
+			findfile(); 
+			System.out.println("java code!");
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
