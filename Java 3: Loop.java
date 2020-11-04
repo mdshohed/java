5ef9b5910e21627be90efcba5c7ce03bef635f8d
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-04 11:11:49 +0600

    Update Java 3: Loop.java

--- Java 3: Loop.java
+++ Java 3: Loop.java
@@ -1,32 +1,24 @@
-
 package problem;
 
 import java.util.*;
 public class problem {
     public static void main(String[] args) {
-        
-        //for loop
-        int n = 10; 
+        int[] ara = {3,5,7,3,6}; 
+        int n = 5;
         for (int i = 0; i<n; i++) {
-            System.out.println("value :"+i);
+            System.out.println("Array vlaue "+i+": "+ara[i]);
         }
-         System.out.println("\n");
-        // while loop 
-        int i = 0;
-        while ( i<n) {
-            System.out.println("value: "+i);
-            i++;
+        
+        n = 0; 
+        while ( n!=10) {
+            System.out.println("while value: " +n);
+            n++; 
         }
-        System.out.println("\n");
 
-
-        // do while loop 
-        i = 0; 
-        do{
-             i++;
-             if ( i%2==0) {
-               System.out.println("do while: "+i);
-             }
-        }while(i<=n);
+        n = 0; 
+        do {
+           System.out.println("do while value: "+ n);
+           n++; 
+        }while( n!=10); 
     }   
 }
